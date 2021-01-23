5d9fddec427e7438fa8681f840303a57181ac7d6
Mohamed Saleh <32750050+salehmoh@users.noreply.github.com>
2021-01-23 01:00:32 -0500

    Add main functionality
    * Added the main functionily of the site with simple styles.
    * Also cleaned up unnecessary files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [url, setUrl] = useState("");
+  let [branchName, setBranchName] = useState("master");
+
+  function handleBranchNameChange(event) {
+    let changedBranchName = event.target.value;
+
+    if(!changedBranchName || changedBranchName.length === 0)
+      return setBranchName("master");
+
+    setBranchName(event.target.value);
+  }
+
+  function handleUrlChange(event) {
+    let changedUrl = event.target.value;
+
+    if(!changedUrl || changedUrl.trim().length === 0)
+      return setUrl("");
+
+    setUrl(changedUrl);
+  }
+
+  function displayConvertedUrl() {
+    if(!url || url.length === 0)
+      return "";
+
+    return url.replace("/commit/", "/compare/").concat(`..${branchName}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>GitHub History Compare</h1>
+      <p>
+        A simple conversion on an older GitHub <u><i>commit</i></u> url to a <u><i>compare</i></u> url against your <strong>branch's latest</strong> commit or <strong>specific</strong> commit ID. <br/>
+        <br />
+        For more info, check out: <br />
+        <a className="info" href="https://docs.github.com/en/github/committing-changes-to-your-project/comparing-commits">
+          https://docs.github.com/en/github/committing-changes-to-your-project/comparing-commits
         </a>
-      </header>
+      </p>
+
+      <label>
+        Compared Branch Name or Commit ID: 
+        <input type="text" placeholder="Default: master" onChange={handleBranchNameChange} onKeyUp={handleBranchNameChange} />
+      </label>
+
+      <label className="commit-url-label">
+        GitHub Commit url: 
+        <input type="text" placeholder="e.g.: https://github.com/<COMPANY>/<PROJECT-NAME>/commit/<BASE-COMMIT-ID>" onChange={handleUrlChange} onKeyUp={handleUrlChange} />
+      </label>
+      <br />
+
+      Converted URL: <br />
+      <a className="result" href={displayConvertedUrl()}>{displayConvertedUrl()}</a>
     </div>
   );
 }
